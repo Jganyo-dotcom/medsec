0c30dac0255ce3ff718b287955b080458c943257
Jganyo-dotcom <elikemejay@gmail.com>
2026-04-08 17:39:47 +0000

    Fix hospitalCreation controller

--- src/routes/managerRoutes/m.js
+++ src/routes/managerRoutes/m.js
@@ -22,10 +22,10 @@ router.get("/get-hospitals", getAllHospitals); // get all
 router.delete("/delete-hospital/:id", deleteHospitalById); // delete hospital
 router.patch("/disable-hospital/:id", disableHospital); // disable hospital
 ///////
-router.get("/get-inactive-hospitals", IsactiveHospital); // to get the inactive hospital
+router.get("/get-active-hospitals", IsactiveHospital); // to get the inactive hospital
 router.get("/get-inactive-hospitals", getInactiveHospitals); //to get the inactive hospital
 router.get("/send-hospital-details/:hospitalId", sendHospitalDetails); // send hospital details to admin
-router.get("/revoke-access/:id", revokeHospitalAdminAccess); // revoke hospital
+router.get("/revoke-access/:Id", revokeHospitalAdminAccess); // revoke hospital
 // router.get("/total-staff", getTotalStaff); // revoke hospital
 
 module.exports = router;
