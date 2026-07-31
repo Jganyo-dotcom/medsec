a6232205b520cb197ea59bd53118f2b5dc628b9b
James Ganyo <elikemejay@gmail.com>
2026-07-31 16:19:54 +0000

    Update m.js

--- src/routes/managerRoutes/m.js
+++ src/routes/managerRoutes/m.js
@@ -39,7 +39,7 @@ const { OnlySuperiorManager, higherAuth2, higherAuth } = require("../../middlewe
 const router = express.Router();
 router.get("/managers", authmiddleware,higherAuth2, getAllManagers); // get all
 router.get("/pending-managers", authmiddleware,OnlySuperiorManager, getAllPotentialManagers);
-router.get("/hospitals/:id", authmiddleware,higherAuth2, getHospitalById);
+router.get("/hospitals/:id", authmiddleware,higherAuth, getHospitalById);
 router.patch("/approve-manager/:id", authmiddleware, OnlySuperiorManager,approveManager);
 router.patch("/approve-reset/:id", authmiddleware,OnlySuperiorManager, approveManagerCredentials);
 router.get("/pending-resets", authmiddleware,higherAuth2, resetManagerPasswordReset);
